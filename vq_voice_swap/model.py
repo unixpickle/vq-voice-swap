fb0058d2263877b275ddd6bed1b110b1111f445d
Alex Nichol <unixpickle@gmail.com>
2021-01-07 19:56:55 -0500

    add extra resblocks to WaveGradEncoder
    Hopefully with more encoder compute, the model will uncover more
    relevant linguistic features.

--- vq_voice_swap/model.py
+++ vq_voice_swap/model.py
@@ -104,11 +104,11 @@ class WaveGradEncoder(nn.Module):
         self.cond_channels = cond_channels
         self.d_blocks = nn.Sequential(
             nn.Conv1d(1, 32, 5, padding=2),
-            DBlock(32, 128, 4),
-            DBlock(128, 128, 2),
-            DBlock(128, 256, 2),
-            DBlock(256, 512, 2),
-            DBlock(512, cond_channels, 2),
+            DBlock(32, 128, 4, extra_blocks=1),
+            DBlock(128, 128, 2, extra_blocks=1),
+            DBlock(128, 256, 2, extra_blocks=1),
+            DBlock(256, 512, 2, extra_blocks=1),
+            DBlock(512, cond_channels, 2, extra_blocks=1),
         )
 
     def forward(self, x, use_checkpoint=False):
