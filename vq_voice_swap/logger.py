c42d3ddf7388261048ccee235a3199e2403df423
Alex Nichol <unixpickle@gmail.com>
2021-01-14 17:06:47 -0500

    resume logger step index from file

--- vq_voice_swap/logger.py
+++ vq_voice_swap/logger.py
@@ -4,18 +4,22 @@ class Logger:
     """
 
     def __init__(self, out_filename: str, resume: bool = False):
+        self.start_step = 0
         if resume:
             self.out_file = open(out_filename, "a+")
-            # TODO: get the latest step in the log file and add it
-            # to future log messages when resuming.
+            self.out_file.seek(0)
+            lines = [x for x in self.out_file.readlines() if x.startswith("step ")]
+            if len(lines):
+                self.start_step = int(lines[-1].split(" ")[1].split(":")[0])
         else:
             self.out_file = open(out_filename, "w+")
 
     def log(self, step: int, **kwargs):
         fields = " ".join(f"{k}={v:.05f}" for k, v in kwargs.items())
-        print(f"step {step}: {fields}")
-        self.out_file.write(f"step {step}: {fields}\n")
+        log_line = f"step {step + self.start_step}: {fields}"
+        self.out_file.write(log_line)
         self.out_file.flush()
+        print(log_line)
 
     def close(self):
         self.out_file.close()
