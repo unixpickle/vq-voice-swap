c3c3f59821e6c0fb6ed5d5e563b7ed96fbde4a6f
Alex Nichol <unixpickle@gmail.com>
2021-01-15 15:39:32 -0500

    API to read from log format

--- vq_voice_swap/logger.py
+++ vq_voice_swap/logger.py
@@ -1,3 +1,33 @@
+from typing import TextIO, Union
+
+
+def read_log(log_reader: Union[str, TextIO]):
+    """
+    Read entries in a log file as dicts.
+
+    Returns an iterator over (step, dict) pairs.
+    """
+    line_idx = 0
+    while True:
+        line = log_reader.readline().rstrip()
+        line_idx += 1
+        if not line:
+            break
+        try:
+            if not line.startswith("step "):
+                raise ValueError
+            step_str, kv_str = line[5:].split(": ")
+            step_idx = int(step_str)
+            kv_strs = kv_str.split(" ")
+            kvs = {}
+            for kv_str in kv_strs:
+                k_str, v_str = kv_str.split("=")
+                kvs[k_str] = float(v_str)
+        except ValueError:
+            raise ValueError(f"unexpected format at line {line_idx}")
+        yield step_idx, kvs
+
+
 class Logger:
     """
     Log training iterations to a file and to standard output.
