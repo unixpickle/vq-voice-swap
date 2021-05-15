1d695de532cf7a71694fc9dd48d61a7271c5fa33
Alex Nichol <unixpickle@gmail.com>
2021-05-15 08:57:30 -0400

    cascaded conditional predictor

--- vq_voice_swap/vq_vae.py
+++ vq_voice_swap/vq_vae.py
@@ -1,6 +1,6 @@
-from abc import abstractmethod
+from abc import abstractmethod, abstractproperty
 import os
-from typing import Any
+from typing import Any, Dict
 
 import torch
 import torch.nn as nn
@@ -20,7 +20,6 @@ class VQVAE(nn.Module):
         self,
         encoder: nn.Module,
         vq: VQ,
-        predictor: Predictor,
         vq_loss: VQLoss,
         diffusion: Diffusion,
         num_labels: int,
@@ -28,7 +27,6 @@ class VQVAE(nn.Module):
         super().__init__()
         self.encoder = encoder
         self.vq = vq
-        self.predictor = predictor
         self.vq_loss = vq_loss
         self.diffusion = diffusion
         self.num_labels = num_labels
@@ -41,9 +39,12 @@ class VQVAE(nn.Module):
         :param labels: an [N] Tensor of integer labels.
         :return: a dict containing the following keys:
                  - "vq_loss": loss for the vector quantization layer.
-                 - "mse": loss for the diffusion decoder.
+                 - "mse": mean loss for all the predictors.
                  - "ts": a 1-D float tensor of the timesteps per batch entry.
-                 - "mses": a 1-D tensor of the MSE losses per batch entry.
+                 - "mses": a 1-D tensor of the mean MSE losses per batch entry
+                           for all of the predictors.
+                 - "mses_dict": a map of separate 1-D MSE tensors for each
+                                predictor.
         """
         encoder_out = self.encoder(inputs, **extra_kwargs)
         vq_out = self.vq(encoder_out)
@@ -52,10 +53,14 @@ class VQVAE(nn.Module):
         ts = torch.rand(inputs.shape[0]).to(inputs)
         epsilon = torch.randn_like(inputs)
         noised_inputs = self.diffusion.sample_q(inputs, ts, epsilon=epsilon)
-        predictions = self.predictor(
+        named_preds = self.predictions(
             noised_inputs, ts, cond=vq_out["passthrough"], labels=labels, **extra_kwargs
         )
-        mses = ((predictions - epsilon) ** 2).flatten(1).mean(1)
+        mses_dict = {
+            name: ((predictions - epsilon) ** 2).flatten(1).mean(1)
+            for name, predictions in named_preds.items()
+        }
+        mses = torch.mean(torch.stack(list(mses_dict.values()), axis=0), axis=0)
         mse = mses.mean()
 
         return {
@@ -63,6 +68,7 @@ class VQVAE(nn.Module):
             "mse": mse,
             "ts": ts,
             "mses": mses,
+            "mses_dict": mses_dict,
         }
 
     def encode(self, inputs: torch.Tensor) -> torch.Tensor:
@@ -97,10 +103,30 @@ class VQVAE(nn.Module):
         ).to(codes.device)
         return self.diffusion.ddpm_sample(
             x_T,
-            self.predictor.condition(cond=cond_seq, labels=labels),
+            lambda xs, ts, **kwargs: self.sample_predictor(
+                xs, ts, cond=cond_seq, labels=labels, **kwargs
+            ),
             steps=steps,
             progress=progress,
         )
+
+    @abstractmethod
+    def predictions(
+        self, xs: torch.Tensor, ts: torch.Tensor, **kwargs
+    ) -> Dict[str, torch.Tensor]:
+        """
+        Get epsilon predictions from one or more predictors.
+
+        Each predictor has a name in the resulting dict.
+        """
+
+    @abstractmethod
+    def sample_predictor(
+        self, xs: torch.Tensor, ts: torch.Tensor, **kwargs
+    ) -> torch.Tensor:
+        """
+        Get epsilon predictions from the best predictor of self.predictions().
+        """
 
     @abstractmethod
     def downsample_rate(self):
@@ -114,14 +140,27 @@ class WaveGradVQVAE(VQVAE):
         super().__init__(
             encoder=WaveGradEncoder(base_channels=base_channels),
             vq=VQ(512, 512),
-            predictor=WaveGradPredictor(
-                base_channels=base_channels, num_labels=num_labels
-            ),
             vq_loss=VQLoss(),
             diffusion=Diffusion(ExpSchedule()),
             num_labels=num_labels,
         )
         self.base_channels = base_channels
+        self.cond_predictor = WaveGradPredictor(
+            base_channels=base_channels, num_labels=num_labels
+        )
+
+    def predictions(
+        self, xs: torch.Tensor, ts: torch.Tensor, **kwargs
+    ) -> Dict[str, torch.Tensor]:
+        return dict(cond=self.cond_predictor(xs, ts, **kwargs))
+
+    def sample_predictor(
+        self, xs: torch.Tensor, ts: torch.Tensor, **kwargs
+    ) -> torch.Tensor:
+        """
+        Get epsilon predictions from the best predictor of self.predictions().
+        """
+        return self.predictions(xs, ts, **kwargs)["cond"]
 
     def save(self, path):
         """
@@ -151,3 +190,26 @@ class WaveGradVQVAE(VQVAE):
 
     def downsample_rate(self):
         return 64
+
+
+class CascadeWaveGradVQVAE(WaveGradVQVAE):
+    def __init__(self, num_labels: int, base_channels: int = 32):
+        super().__init__(
+            num_labels=num_labels,
+            base_channels=base_channels,
+        )
+        self.base_predictor = WaveGradPredictor(base_channels=base_channels)
+        self.label_predictor = WaveGradPredictor(
+            base_channels=base_channels,
+            num_labels=num_labels,
+        )
+
+    def predictions(
+        self, xs: torch.Tensor, ts: torch.Tensor, **kwargs
+    ) -> Dict[str, torch.Tensor]:
+        base = self.base_predictor(xs, ts)
+        label = base.detach() + self.label_predictor(xs, ts, labels=kwargs["labels"])
+        cond = label.detach() + self.label_predictor(
+            xs, ts, cond=kwargs["cond"], labels=kwargs["labels"]
+        )
+        return dict(base=base, label=label, cond=cond)
