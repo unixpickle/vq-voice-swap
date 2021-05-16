ea2131e1f1386cd979830c952c40b64936a8bdca
Alex Nichol <unixpickle@gmail.com>
2021-05-16 00:26:31 -0400

    revert FILM changes
    Revert "easy way for film layer to ignore labels"
    
    This reverts commit 5146945a8eb24278a8a02c2d4498b8f34a5cf060.
    
    Revert "another attempted improvement"
    
    This reverts commit 4f8275b7690357e671216ddc96160e2e5ca61924.
    
    Revert "updates to FILM layer"
    
    This reverts commit 3cf6f9deada85c4ff7c83a3920fd8bf31fb8a774.

--- vq_voice_swap/model.py
+++ vq_voice_swap/model.py
@@ -5,7 +5,6 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint, checkpoint_sequential
 
 
@@ -296,11 +295,9 @@ class FILM(nn.Module):
             nn.Conv1d(cond_channels, out_channels, 3, padding=1),
             nn.GELU(),
         )
-        self.time_mix = nn.Linear(out_channels, out_channels)
         self.out_layer = nn.Conv1d(out_channels, out_channels * 2, 3, padding=1)
         if num_labels is not None:
             self.label_emb = nn.Embedding(num_labels, out_channels)
-            self.label_emb_mix = nn.Linear(out_channels, out_channels)
         else:
             self.label_emb = None
 
@@ -319,17 +316,16 @@ class FILM(nn.Module):
         ).to(cond)
         args = t[:, None].to(cond.dtype) * freqs[None]
         embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
-        embedding = self.time_mix(embedding)
         assert (labels is None) == (self.label_emb is None)
         if labels is not None:
-            embedding = embedding + self.label_emb_mix(self.label_emb(labels))
+            embedding = embedding + self.label_emb(labels)
         while len(embedding.shape) < len(cond.shape):
             embedding = embedding[..., None]
         cond_out = self.in_layer(cond)
-        alpha_beta = self.out_layer(F.gelu(embedding + cond_out))
+        alpha_beta = self.out_layer(embedding + cond_out)
         alpha, beta = torch.split(alpha_beta, self.out_channels, dim=1)
 
-        return inputs * (alpha + 1) + beta
+        return inputs * alpha + beta
 
 
 class NCTLayerNorm(nn.Module):
