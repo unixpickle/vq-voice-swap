36a8fff721d55f56ffd04ee89c4eb46c617a0f49
Alex Nichol <unixpickle@gmail.com>
2021-05-19 10:01:29 -0400

    finish draft of UNet port

--- vq_voice_swap/unet.py
+++ vq_voice_swap/unet.py
@@ -2,7 +2,7 @@
 Adapted from https://github.com/openai/guided-diffusion/blob/b16b0a180ffac9da8a6a03f1e78de8e96669eee8/guided_diffusion/unet.py.
 """
 
-from typing import Optional
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
@@ -11,21 +11,128 @@ import torch.nn.functional as F
 from .model import Predictor, TimeEmbedding
 
 
-class Resize(nn.Module):
-    def __init__(self, scale_factor: float):
-        self.scale_factor = scale_factor
+class UNet(Predictor):
+    def __init__(
+        self,
+        channels: int,
+        channel_mult: Tuple[int] = (1, 1, 2, 2, 2, 4, 4, 8),
+        depth_mult: int = 2,
+        cond_channels: Optional[int] = None,
+        num_classes: Optional[int] = None,
+        in_channels: int = 1,
+        out_channels: int = 1,
+    ):
+        super().__init__()
+        self.channels = channels
+        self.channel_mult = channel_mult
+        self.depth_mult = depth_mult
+        self.cond_channels = cond_channels
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+        self.out_channels = out_channels
 
-    def forward(self, x):
-        if self.scale_factor == 1.0:
-            return x
-        if self.scale_factor < 1.0:
-            down_factor = int(round(1 / self.scale_factor))
-            assert (
-                float(1 / down_factor - self.scale_factor) < 1e-5
-            ), "scale factor must be integer or 1/integer"
-            return F.avg_pool1d(x, down_factor)
-        else:
-            return F.interpolate(x, scale_factor=self.scale_factor)
+        embed_dim = channels * 4
+        self.time_embed = TimeEmbedding(embed_dim)
+        if num_classes is not None:
+            self.class_embed = nn.Embedding(num_classes, embed_dim)
+        if cond_channels is not None:
+            self.cond_proj = nn.Conv1d(cond_channels, channels)
+
+        self.in_conv = nn.Conv1d(in_channels, channels, 1)
+
+        skip_channels = [channels]
+        cur_channels = channels
+
+        self.down_blocks = nn.ModuleList([])
+        for depth, mult in enumerate(channel_mult):
+            for _ in range(depth_mult):
+                self.down_blocks.append(
+                    ResBlock(
+                        channels=cur_channels,
+                        emb_channels=embed_dim,
+                        out_channels=mult * channels,
+                    )
+                )
+                ch = mult * channels
+                skip_channels.append(ch)
+            if depth != len(channel_mult) - 1:
+                self.down_blocks.append(
+                    ResBlock(
+                        channels=cur_channels,
+                        emb_channels=cur_channels,
+                        scale_factor=0.5,
+                    ),
+                )
+                skip_channels.append(ch)
+
+        self.middle_blocks = nn.ModuleList(
+            [
+                ResBlock(
+                    channels=cur_channels,
+                    emb_channels=embed_dim,
+                )
+                for _ in range(2)
+            ]
+        )
+
+        self.up_blocks = nn.ModuleList([])
+        for depth, mult in enumerate(channel_mult)[::-1]:
+            for _ in range(depth_mult + 1):
+                in_ch = skip_channels.pop()
+                self.up_blocks.append(
+                    ResBlock(
+                        channels=cur_channels + in_ch,
+                        emb_channels=embed_dim,
+                        out_channels=mult * channels,
+                    )
+                )
+                cur_channels = mult * channels
+            if depth:
+                self.up_blocks.append(
+                    ResBlock(
+                        channels=cur_channels, emb_channels=embed_dim, scale_factor=2.0
+                    )
+                )
+
+        self.out = nn.Sequential(
+            norm_act(channels),
+            nn.Conv1d(channels, out_channels, 3, padding=1),
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        ts: torch.Tensor,
+        cond: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        assert (labels is None) == (
+            self.num_classes is None
+        ), "must provide labels if and only if model is class conditional"
+        assert (cond is None) == (
+            self.cond_channels is None
+        ), "must provide cond sequence if and only if model is conditional"
+
+        emb = self.time_embed(ts)
+        if labels is not None:
+            emb = emb + self.class_embed(labels)
+
+        h = self.in_conv(x)
+        if cond is not None:
+            h = h + F.interpolate(self.cond_proj(cond), h.shape[-1])
+
+        skips = []
+        for block in self.down_blocks:
+            h = block(h, emb)
+            skips.append(h)
+        for i, block in self.up_blocks:
+            # No skip connection for upsampling block.
+            if i % (self.depth_mult + 2) != self.depth_mult + 1:
+                h = torch.cat(h, skips.pop(), axis=1)
+            h = block(h)
+
+        h = self.out(h)
+        return h
 
 
 class ResBlock(nn.Module):
@@ -73,6 +180,23 @@ class ResBlock(nn.Module):
         h = h * (cond_a + 1) + cond_b
         h = self.post_cond(h)
         return self.skip(x) + h
+
+
+class Resize(nn.Module):
+    def __init__(self, scale_factor: float):
+        self.scale_factor = scale_factor
+
+    def forward(self, x):
+        if self.scale_factor == 1.0:
+            return x
+        if self.scale_factor < 1.0:
+            down_factor = int(round(1 / self.scale_factor))
+            assert (
+                float(1 / down_factor - self.scale_factor) < 1e-5
+            ), "scale factor must be integer or 1/integer"
+            return F.avg_pool1d(x, down_factor)
+        else:
+            return F.interpolate(x, scale_factor=self.scale_factor)
 
 
 def norm_act(ch: int) -> nn.Module:
