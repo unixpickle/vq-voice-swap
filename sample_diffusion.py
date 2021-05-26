1e1403d273b3f5447adfe7037e5cf028c35dc5f8
Alex Nichol <unixpickle@gmail.com>
2021-05-26 08:35:39 -0400

    classifier-conditional sampling

--- sample_diffusion.py
+++ sample_diffusion.py
@@ -5,7 +5,9 @@ Train an unconditional diffusion model on waveforms.
 import argparse
 
 import torch
+import torch.nn.functional as F
 
+from vq_voice_swap.classifier import Classifier
 from vq_voice_swap.dataset import ChunkWriter
 from vq_voice_swap.diffusion import Diffusion
 from vq_voice_swap.schedule import ExpSchedule
@@ -23,9 +25,28 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
+    if args.classifier_path:
+        classifier = Classifier.load(args.classifier_path)
+
+        def cond_fn(x, ts):
+            with torch.enable_grad():
+                x = x.detach().clone().requires_grad_()
+                logits = classifier(x, ts, use_checkpoint=args.grad_checkpoint)
+                logprobs = F.log_softmax(logits, dim=-1)
+                grads = torch.autograd.grad(logprobs[:, args.classifier_class], x)[0]
+                return grads.detach()
+
+    else:
+        cond_fn = None
+
     x_T = torch.randn(1, 1, 64000, device=device)
     sample = diffusion.ddpm_sample(
-        x_T, model, args.sample_steps, progress=True, constrain=args.constrain
+        x_T,
+        model,
+        args.sample_steps,
+        progress=True,
+        constrain=args.constrain,
+        cond_fn=cond_fn,
     )
 
     writer = ChunkWriter(args.sample_path, 16000)
@@ -43,6 +64,10 @@ def arg_parser():
     parser.add_argument("--constrain", action="store_true")
     parser.add_argument("--checkpoint-path", default="model_diffusion.pt", type=str)
     parser.add_argument("--sample-path", default="sample.wav", type=str)
+    parser.add_argument("--grad-checkpoint", action="store_true")
+    parser.add_argument("--classifier-path", default=None, type=str)
+    parser.add_argument("--classifier-scale", default=1.0, type=float)
+    parser.add_argument("--classifier-class", default=0, type=int)
     return parser
 
 
