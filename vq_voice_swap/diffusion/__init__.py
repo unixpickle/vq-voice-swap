048e59eebc402921ef5445cc5bcc66ca2dba2925
Alex Nichol <unixpickle@gmail.com>
2021-05-30 12:56:07 -0400

    import

--- vq_voice_swap/diffusion/__init__.py
+++ vq_voice_swap/diffusion/__init__.py
@@ -1,5 +1,5 @@
 from .diffusion import Diffusion
 from .make import make_schedule
-from .schedule import Schedule, ExpSchedule
+from .schedule import CosSchedule, Schedule, ExpSchedule
 
-__all__ = ["Diffusion", "make_schedule", "Schedule", "ExpSchedule"]
+__all__ = ["Diffusion", "make_schedule", "CosSchedule", "Schedule", "ExpSchedule"]
