1a03a4fa15fc5f670aa86026af2a9a0de566922e
Alex Nichol <unixpickle@gmail.com>
2021-06-12 18:57:09 -0400

    train / finetune a class-conditional diffusion model

--- train_diffusion.py
+++ train_diffusion.py
@@ -20,6 +20,13 @@ from vq_voice_swap.util import count_params, repeat_dataset
 def main():
     args = arg_parser().parse_args()
 
+    print("creating data loader...")
+    data_loader, num_labels = create_data_loader(
+        directory=args.data_dir, batch_size=args.batch_size
+    )
+    if not args.class_cond:
+        num_labels = None
+
     if os.path.exists(args.checkpoint_path):
         print("loading from checkpoint...")
         model = DiffusionModel.load(args.checkpoint_path)
@@ -31,8 +38,15 @@ def main():
             base_channels=args.base_channels,
             schedule_name=args.schedule,
             dropout=args.dropout,
+            num_labels=num_labels,
         )
         resume = False
+
+        if args.pretrained_path:
+            print(f"loading from pretrained model: {args.pretrained_path} ...")
+            dm = DiffusionModel.load(args.pretrained_path)
+            num_params = model.load_from_pretrained(dm)
+            print(f"loaded {num_params} pre-trained parameters...")
 
     print(f"total parameters: {count_params(model)}")
 
@@ -49,9 +63,6 @@ def main():
     tracker = LossTracker()
     logger = Logger(args.log_file, resume=resume)
 
-    data_loader, _ = create_data_loader(
-        directory=args.data_dir, batch_size=args.batch_size
-    )
     for i, data_batch in enumerate(repeat_dataset(data_loader)):
         audio_seq = data_batch["samples"][:, None].to(device)
         ts = torch.rand(args.batch_size, device=device)
@@ -84,12 +95,14 @@ def arg_parser():
     parser.add_argument("--base-channels", default=32, type=int)
     parser.add_argument("--dropout", default=0.0, type=float)
     parser.add_argument("--schedule", default="exp", type=str)
+    parser.add_argument("--class-cond", action="store_true")
     parser.add_argument("--lr", default=1e-4, type=float)
     parser.add_argument("--ema-rate", default=0.9999, type=float)
     parser.add_argument("--weight-decay", default=0.0, type=float)
     parser.add_argument("--batch-size", default=8, type=int)
     parser.add_argument("--checkpoint-path", default="model_diffusion.pt", type=str)
     parser.add_argument("--ema-path", default="model_diffusion_ema.pt", type=str)
+    parser.add_argument("--pretrained-path", default=None, type=str)
     parser.add_argument("--save-interval", default=1000, type=int)
     parser.add_argument("--grad-checkpoint", action="store_true")
     parser.add_argument("--log-file", default="train_diffusion_log.txt")
